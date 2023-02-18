c111dbb9edf03df38e214ae36bf082ed3be56434
evr-onen <59825869+evr-onen@users.noreply.github.com>
2023-02-18 12:28:38 +0300

    Revert "a"

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,11 +1,11 @@
-// import { NextResponse } from "next/server"
-// import type { NextRequest } from "next/server"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
-// export function middleware(request: NextRequest) {
-//   const url = request.nextUrl.clone()
-//   console.log(url)
-//   if (url.pathname === "/") {
-//     url.pathname = "/home"
-//     return NextResponse.redirect(url)
-//   }
-// }
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone()
+  console.log(url)
+  if (url.pathname === "/") {
+    url.pathname = "/home"
+    return NextResponse.redirect(url)
+  }
+}
